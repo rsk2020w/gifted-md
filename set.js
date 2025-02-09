b659f910932ed417d4463a3afd55b7f390494e22
rsk2020w <kassimewika@gmail.com>
2025-02-09 20:02:51 +0000

    Update set.js

--- set.js
+++ set.js
@@ -27,7 +27,7 @@ OWNER_NUMBER: process.env.OWNER_NUMBER || "254715206562", // Only 1 owner Number
 OWNER_NAME: process.env.OWNER_NAME || "𝐆𝐈𝐅𝐓𝐄𝐃 𝐓𝐄𝐂𝐇", // Input Yours custom...(Maintain font for Flow)
 PACK_AUTHOR: process.env.PACK_AUTHOR || "𝐆𝐈𝐅𝐓𝐄𝐃 𝐓𝐄𝐂𝐇", // Added // Input Yours custom...
 PACK_NAME: process.env.PACK_NAME || "𝐆𝐈𝐅𝐓𝐄𝐃-𝐌𝐃💜", // Added // Input Yours custom...
-PREFIX: process.env.PREFIX || ".",
+PREFIX: process.env.PREFIX || "#",
 VERSION: process.env.VERSION || "5.0.0",
 ANTILINK: process.env.ANTILINK || "false", //  Enter true to kick automatically or delete to delete without kicking or warn to warn before kicking
 ANTICALL: process.env.ANTICALL || "false",
